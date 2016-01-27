4c35fbf49fbd905ed87a2ea44b1e3b991c1bf570
Gökhan KOCAKAYA <gokhan@doktorsitesi.com>
2016-01-27 10:44:59 +0200

    yaml

--- DependencyInjection/ParseSdkExtension.php
+++ DependencyInjection/ParseSdkExtension.php
@@ -5,6 +5,7 @@ namespace Parse\SdkBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Loader;
 
 /**
@@ -22,8 +23,8 @@ class ParseSdkExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
 
         $container->setParameter('parse_sdk.app_id', $config['parse_sdk_app_id']);
         $container->setParameter('parse_sdk.rest_key', $config['parse_sdk_rest_key']);
